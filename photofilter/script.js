c2649a283e9fa10a54bd0b2bc3e1360e960b6e9f
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2021-04-25 16:34:27 +0300

    feat: add fotofilter

--- photofilter/script.js
+++ photofilter/script.js
@@ -12,3 +12,33 @@ const toggleFullScreenIcn = () =>
   fullscreenBtn.childNodes[1].outerHTML === '<img src="assets/icons/open-fullscreen.svg">'
     ? fullscreenBtn.childNodes[1].outerHTML = '<img src="assets/icons/close-fullscreen.svg">'
     : fullscreenBtn.childNodes[1].outerHTML = '<img src="assets/icons/open-fullscreen.svg">';
+
+// // Loader
+
+// const fileInput = document.querySelector('input[type="file"]');
+// const photoContainer = document.querySelector('.photo-container');
+
+// fileInput.addEventListener('change', function (e) {
+//   const actualFile = fileInput.files[0];
+//   const reader = new FileReader();
+//   reader.onload = () => {
+//     const img = new Image();
+//     img.src = reader.result;
+//     photoContainer.innerHTML = "";
+//     photoContainer.append(img);
+//   }
+//   reader.readAsDataURL(actualFile);
+// });
+
+// Photo-redactor
+
+let controls = document.querySelector('.photo-redactor');
+let inputs = controls.querySelectorAll('.photo-redactor input');
+
+function handleUpdate() {
+  const sizeType = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + sizeType);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
