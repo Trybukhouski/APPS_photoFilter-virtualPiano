c6ec9d7c4cc0fab97a06c2327211f21c0138db68
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2021-04-26 12:44:55 +0300

    refactor: improvements

--- photofilter/script.js
+++ photofilter/script.js
@@ -7,6 +7,12 @@ const images = document.querySelectorAll('.image');
 let controls = document.querySelector('.photo-redactor');
 let inputs = controls.querySelectorAll('.photo-redactor input');
 const fullscreenBtn = document.querySelector(".fullscreen");
+const blurRange = document.getElementById('blur');
+const sepiaRange = document.getElementById('sepia');
+const invertRange = document.getElementById('inver');
+const saturateRange = document.getElementById('saturate');
+const hueRotateRange = document.getElementById('hue-rotate');
+let filterValueBlur = document.querySelector('.filter-value_blur');
 
 // Fullscreen option
 
@@ -20,22 +26,6 @@ const toggleFullScreenIcn = () =>
   fullscreenBtn.childNodes[1].outerHTML === '<img src="assets/icons/open-fullscreen.svg">'
     ? fullscreenBtn.childNodes[1].outerHTML = '<img src="assets/icons/close-fullscreen.svg">'
     : fullscreenBtn.childNodes[1].outerHTML = '<img src="assets/icons/open-fullscreen.svg">';
-
-// Loader
-
-// const fileInput = document.querySelector('input[type="file"]');
-
-// fileInput.addEventListener('change', function (e) {
-//   const actualFile = fileInput.files[0];
-//   const reader = new FileReader();
-//   reader.onload = () => {
-//     const img = new Image();
-//     img.src = reader.result;
-//     sliderLine.innerHTML = "";
-//     sliderLine.append(img);
-//   }
-//   reader.readAsDataURL(actualFile);
-// });
 
 // Photo-redactor
 
@@ -66,11 +56,6 @@ function nextPhoto() {
 resetBtn.addEventListener('click', resetFilters);
 
 function resetFilters() {
-  const blurRange = document.getElementById('blur');
-  const sepiaRange = document.getElementById('sepia');
-  const invertRange = document.getElementById('inver');
-  const saturateRange = document.getElementById('saturate');
-  const hueRotateRange = document.getElementById('hue-rotate');
   doc[0].style = "--blur:0px; --sepia:0%; --invert:0%; --saturate:100%; --hue-rotate:0deg;";
   blurRange.value = "0";
   sepiaRange.value = "0";
@@ -78,3 +63,21 @@ function resetFilters() {
   saturateRange.value = "100";
   hueRotateRange.value = "0";
 }
+
+// Loader
+
+// const fileInput = document.querySelector('input[type="file"]');
+
+// fileInput.addEventListener('change', function (e) {
+//   const actualFile = fileInput.files[0];
+//   const reader = new FileReader();
+//   reader.onload = () => {
+//     const img = new Image();
+//     img.src = reader.result;
+//     sliderLine.innerHTML = "";
+//     sliderLine.append(img);
+//   }
+//   reader.readAsDataURL(actualFile);
+// });
+
+// Download
